79b851cf6103068bb3845558ba8dddc1a8967ccd
amybalala <98797072+amybalala@users.noreply.github.com>
2023-02-20 20:51:18 -0800

    re-upload code

--- chart-1.R
+++ chart-1.R
@@ -1,18 +1,11 @@
 library(ggplot2)
 library(scales)
 
-# Load the data from the link
-tax_data <- read.csv("https://raw.githubusercontent.com/info-201a-wi23/exploratory-analysis-timmythehatter/main/dataExplored.R")
+source("https://raw.githubusercontent.com/info-201a-wi23/exploratory-analysis-timmythehatter/main/dataExplored.R")
 
-# Calculate the total taxes paid per year
-total_taxes <- with(tax_data, tapply(Taxable_Income, Tax_Year, sum))
-total_taxes <- data.frame(year = as.numeric(names(total_taxes)), total_taxes = total_taxes)
-
-# Create a line chart of total taxes paid per year
-ggplot(data = total_taxes, aes(x = year, y = total_taxes, group = 1)) +
-  geom_line(color = "blue") +
-  geom_point(color = "blue", size = 3) +
+ggplot(data = energy_per_year) +
+  geom_line(aes(x = Year, y = energy_per_year)) +
+  labs(title = "U.S. Energy Use Per Year (1960-2020)", x = "Year", y = "Energy Use Per Year (in physical units)") +
   scale_x_continuous(breaks = seq(1960, 2020, 5)) +
-  labs(title = "Total Taxes Paid per Year", x = "Year", y = "Total Taxes")
-
+  scale_y_continuous(labels = label_number_si())
 
