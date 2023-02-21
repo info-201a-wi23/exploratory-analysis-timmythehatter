be9b5ff4ffc79572d565dfc947251b980ebaa17c
shuaisty <shuaijpn@gmail.com>
2023-02-20 21:07:22 -0800

    quick fixes in chart 3

--- Chart-3.R
+++ Chart-3.R
@@ -5,11 +5,6 @@ library(dplyr)
 # Read the data from the CSV file
 taxes <- read.csv("https://raw.githubusercontent.com/info-201a-wi23/exploratory-analysis-timmythehatter/main/STC_Historical_2021_cleaned.csv")
 
-# Remove first row of taxes
-taxes_removed <- taxes%>%slice(-1)
-
-taxes_per_year <- taxes_removed%>%group_by(Year)%>%summarize(average_taxes = mean(as.double(C105), na.rm = TRUE))
-
 # Create a scatter plot
 ggplot(data = taxes_per_year, aes(x = Year, y = average_taxes)) +
   geom_point(alpha = 2) +
